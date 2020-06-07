1eed74d17784b68a4f9b0b42eb123306b2106d79
uroca <luismurbina@mac.com>
2020-06-06 23:36:40 -0400

    Refactoring: All the various parts of the main window are instance variables in the parent class.  This can be used now to updated a part of the GUI from a different widget in the GUI.

--- dupe_photo_finder/view.py
+++ dupe_photo_finder/view.py
@@ -6,42 +6,37 @@ import os
 
 class MenuBar:
 
-    def __init__(self, parent, controller, listbox_dupe_sets, status_bar):
+    def __init__(self, parent):
         self.parent = parent
-        self.controller = controller
-        self.listbox_dupe_sets = listbox_dupe_sets
-        self.status_bar = status_bar
-        menu_bar = Menu(parent)
+        menu_bar = Menu(self.parent.gui)
         self.create_actions_menu(menu_bar)
-        self.parent.config(menu=menu_bar)
+        self.parent.gui.config(menu=menu_bar)
 
     def _quit(self):
         self.parent.quit()
-        self.parent.destroy()
-        sys.exit()
 
     def add_search_directory(self):
         new_directory = filedialog.askdirectory(title = 'What directory should I search for duplicate photos?')
         if new_directory:
-            self.controller.add_directory_to_search(new_directory)
-            self.status_bar.update_message("Directories to search: " + str(len(self.controller.directories_to_search)))
+            self.parent.controller.add_directory_to_search(new_directory)
+            self.parent.status_bar.update_message("Directories to search: " + str(len(self.parent.controller.directories_to_search)))
 
     def process_search_request(self):
         progress_window = ProgressWindow(self.parent)
         progress_window.update_message('Searching for files')
-        self.status_bar.update_message('Searching for duplicates')
+        self.parent.status_bar.update_message('Searching for duplicates')
         try:
-            duplicate_sets = self.controller.request_search(progress_window)
+            duplicate_sets = self.parent.controller.request_search(progress_window)
         except ValueError as e:
             messagebox.showerror('Error', str(e))
             print( str(e))
         finally:
             progress_window.close_window()
 
-        self.status_bar.clear_message()
+        self.parent.status_bar.clear_message()
         if duplicate_sets and len(duplicate_sets)>0:
             for index in range(0, len(duplicate_sets)):
-                self.listbox_dupe_sets.add_item_to_list(index)
+                self.parent.listbox_dupe_sets.add_item_to_list(index)
 
 
     def create_actions_menu(self, menu_bar):
@@ -80,60 +75,61 @@ class ProgressWindow:
         self.label.pack(padx=12, pady=12)
         self.progress_bar = ttk.Progressbar(self.window)
         self.progress_bar.pack(fill='x', padx=12, pady=12)
-        self.parent.update_idletasks()
+        self.parent.gui.update_idletasks()
 
     def update_message(self, message):
         self.label.configure(text = message)
-        self.parent.update_idletasks()
+        self.parent.gui.update_idletasks()
 
     def clear_message(self):
         self.update_message('')
-        self.parent.update_idletasks()
+        self.parent.gui.update_idletasks()
 
     def change_max_value_progress_bar(self, value):
         self.progress_bar['maximum'] = value
-        self.parent.update_idletasks()
+        self.parent.gui.update_idletasks()
 
     def update_progress_bar(self, value):
         self.progress_bar['value'] = value
-        self.parent.update_idletasks()
+        self.parent.gui.update_idletasks()
 
     def close_window(self):
         self.window.destroy()
-        self.parent.update_idletasks()
+        self.parent.gui.update_idletasks()
 
 
 class ListsWithSets:
 
-    def __init__(self, parent, listbox_of_files):
+    def __init__(self, parent):
         self.parent = parent
-        self.listbox_of_files = listbox_of_files
-        self.listbox = tk.Listbox(parent, selectmode = 'browse', borderwidth=0, highlightthickness=0, background='grey')
+        self.listbox = tk.Listbox(self.parent.frame1, selectmode = 'browse', borderwidth=0, highlightthickness=0, background='grey')
         bold = tk.font.Font(weight='bold')
         self.listbox.config(font=bold)
         self.listbox.pack(side='top')
 
     def add_item_to_list(self, item):
         self.listbox.insert('end', item)
-        self.parent.update_idletasks()
+        self.parent.frame1.update_idletasks()
 
 
 class ListboxFiles:
 
     def __init__(self, parent):
         self.parent = parent
-        self.listbox = tk.Listbox(parent, selectmode = 'single', borderwidth=0, highlightthickness=0, background='Blue')
+        self.listbox = tk.Listbox(self.parent.frame2, selectmode = 'single', borderwidth=0, highlightthickness=0, background='Blue')
         self.listbox.pack(side='top')
 
     def add_item_to_list(self, item):
         self.listbox.insert('end', item)
-        self.parent.update_idletasks()
+        self.parent.frame2.update_idletasks()
 
 
 class Parent:
+
     def __init__(self, controller):
         self.controller = controller
         self.gui = tk.Tk()
+
         self.gui.geometry('1000x720+80+80')
         self.gui.title('Duplicate Photo Detector')
         self.gui.grid_rowconfigure(0, weight=100)
@@ -141,14 +137,22 @@ class Parent:
         self.gui.grid_columnconfigure(0, weight=1)
         self.gui.grid_columnconfigure(1, weight=6)
         self.gui.grid_columnconfigure(2, weight=6)
-        frame1 = Frame(self.gui, background="grey")
-        frame2 = Frame(self.gui)
-        frame3 = Frame(self.gui, background='Blue')
-        frame1.grid(row=0, column=0, sticky="nsew")
-        frame2.grid(row=0, column=1, sticky="nsew")
-        frame3.grid(row=0, column=2, sticky="nsew")
-        status_bar = StatusBar(self.gui)
-        listbox_of_files = ListboxFiles(frame2)
-        listbox_dupe_sets = ListsWithSets(frame1, listbox_of_files)
-        MenuBar(self.gui, self.controller, listbox_dupe_sets, status_bar)
+
+        self.frame1 = Frame(self.gui, background="grey")
+        self.frame2 = Frame(self.gui)
+        self.frame3 = Frame(self.gui, background='Blue')
+        self.frame1.grid(row=0, column=0, sticky="nsew")
+        self.frame2.grid(row=0, column=1, sticky="nsew")
+        self.frame3.grid(row=0, column=2, sticky="nsew")
+
+        self.status_bar = StatusBar(self.gui)
+        self.listbox_of_files = ListboxFiles(self)
+        self.listbox_dupe_sets = ListsWithSets(self)
+        self.menu_bar = MenuBar(self)
+
         self.gui.mainloop()
+
+    def quit(self):
+        self.gui.quit()
+        self.gui.destroy()
+        sys.exit()
